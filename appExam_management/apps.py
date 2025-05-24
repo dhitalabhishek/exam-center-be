69ec1eab08903b1a2e11dc6b16c10ce998c9fac5
Scelester <nabinpauudel664@gmail.com>
2025-05-24 19:54:53 +0545

    re-initialized Project on Django

---
+++ appExam_management/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppexamManagementConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "appExam_management"
---
+++ appSeatPlanning/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppseatplanningConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "appSeatPlanning"
